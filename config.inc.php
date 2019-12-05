c5ce5c75c874715bff48c5fc22be86f10e560b07
danmed <danmed@gmail.com>
2019-12-05 16:21:03 +0000

    Create config.inc.php

---
+++ config.inc.php
@@ -0,0 +1,8 @@
+<?PHP
+/* MYSQL Configuration */
+$DBServer       =       '192.168.2.75';
+$DBUser         =       'username';
+$DBPassword     =       'password';
+$DBName         =       'tasmobackup';
+$Retain		=	'7';
+?>

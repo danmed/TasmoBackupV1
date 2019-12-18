24338970060dbab5bd3c2ed8145c5b80bc398357
danmed <danmed@gmail.com>
2019-12-18 15:42:27 +0000

    Update install.php

--- install.php
+++ install.php
@@ -1,10 +1,18 @@
 <?PHP
+$old = umask(0);
 if (!file_exists('data/backups')) {
     mkdir('data/backups', 0777, true);
-    echo "Backup location created";
+    umask($old);
+    echo "Backup location created<br>";
 }
 if (!file_exists('data/tasmobackup.db')) {
-	copy('tasmobackup.db', 'data/tasmobackup.db')
- echo "Database created";
+        copy('tasmobackup.db', 'data/tasmobackup.db');
+ echo "Database created<br>";
 }
+if (!file_exists('data/config.inc.php.example')) {
+    copy('config.inc.php.example', 'data/config.inc.php');
+    umask($old);
+    echo "Config file created";
+}
+
 ?>

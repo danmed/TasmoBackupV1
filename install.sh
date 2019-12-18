131b6841a74b3cda406784b1fac878dea194bacc
danmed <danmed@gmail.com>
2019-12-18 16:46:24 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-copy /var/www/html/tasmobackup.db /var/www/html/data/tasmobackup.db
-copy /var/www/html/config.inc.php.example /var/www/html/data/config.inc.php
+cp /var/www/html/tasmobackup.db /var/www/html/data/tasmobackup.db
+cp /var/www/html/config.inc.php.example /var/www/html/data/config.inc.php
 mkdir /var/www/html/data/backups
 chmod 777 /var/www/html/data/backups

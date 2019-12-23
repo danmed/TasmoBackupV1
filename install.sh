cdabef0aaef61a2db0e6c26d42ba249484e717bd
danmed <danmed@gmail.com>
2019-12-23 13:48:41 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,3 +12,7 @@ if [ ! -f var/www/html/data/backups ]; then
     mkdir /var/www/html/data/backups
     chmod 777 /var/www/html/data/backups
 fi
+
+sed -i "s/mysqlserver/$MYSQL_SERVER/g" /var/www/html/data/config.inc.php
+sed -i "s/mysqlusername/$MYSQL_USERNAME/g" /var/www/html/data/config.inc.php
+sed -i "s/mysqlpassword/$MYSQL_PASSWORD/g" /var/www/html/data/config.inc.php

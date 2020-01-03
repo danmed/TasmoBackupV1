cbd6f672fa49b80a401b0eae3bec08b1c8482090
danmed <danmed@gmail.com>
2020-01-03 13:51:57 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-if [ ! -f /var/www/html/data/tasmobackup.db ]; then
-    cp /var/www/html/tasmobackup.db /var/www/html/data/tasmobackup.db
-fi
-
 if [ ! -f /var/www/html/data/config.inc.php ]; then
     cp /var/www/html/config.inc.php.example /var/www/html/data/config.inc.php
 fi

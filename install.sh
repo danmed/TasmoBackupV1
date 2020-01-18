db7b6ef0f6f8cca2afa0e0ef07ded47e626afa18
danmed <danmed@gmail.com>
2020-01-18 20:38:35 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,3 +17,7 @@ fi
 sed -i "s/mysqlserver/$MYSQL_SERVER/g" /var/www/html/data/config.inc.php
 sed -i "s/mysqlusername/$MYSQL_USERNAME/g" /var/www/html/data/config.inc.php
 sed -i "s/mysqlpassword/$MYSQL_PASSWORD/g" /var/www/html/data/config.inc.php
+sed -i "s/dbtype/$DBTYPE/g" /var/www/html/data/config.inc.php
+sed -i "s/dbname/$DBNAME/g" /var/www/html/data/config.inc.php
+
+

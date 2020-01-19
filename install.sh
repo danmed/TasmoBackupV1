e20a51b016af07cec4a68847be51d7119f94f7ab
danmed <danmed@gmail.com>
2020-01-19 09:17:52 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f /var/www/html/data/settings.inc.php ]; then
     cp /var/www/html/settings.inc.php /var/www/html/data/settings.inc.php
-    chmod 777 /var/www/html/data/backups
+    chmod 777 /var/www/html/data/backups/settings.inc.php
 fi
 
 if [ ! -f /var/www/html/data/config.inc.php ]; then

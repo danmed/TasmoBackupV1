b89b3ec8bda2e6c912f8fd8eb64b6704fb211a91
danmed <danmed@gmail.com>
2020-01-22 16:00:53 +0000

    Update runalways.sh

--- runalways.sh
+++ runalways.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 sed -i "s/mysqlserver/$MYSQL_SERVER/g" /var/www/html/data/config.inc.php
 sed -i "s/mysqlusername/$MYSQL_USERNAME/g" /var/www/html/data/config.inc.php
 sed -i "s/mysqlpassword/$MYSQL_PASSWORD/g" /var/www/html/data/config.inc.php

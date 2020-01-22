3e69372e947c07cdd09e8a9c6ccdc573bf7d7740
danmed <danmed@gmail.com>
2020-01-22 16:56:37 +0000

    Update listbackups.php

--- listbackups.php
+++ listbackups.php
@@ -44,7 +44,7 @@ $(document).ready(function() {
     <div class="container">                                                                                  
     <table class="table table-striped table-bordered" id="status">                                          
     <thead>                                                                                                  
-	    <tr><th colspan="9"><center><b><a href="index.php">TasmoBackup</a><a href="settings.php"><img src="settings.png"></a></th></tr>                                                
+	    <tr><th colspan="9"><center><b><a href="index.php"><?PHP echo $name; ?></a></th></tr>                                                
         <tr><th><b>VERSION</th><th>DATE</th><th>FILE</th></tr>
     </thead>                                                                                                
     <tbody>  
@@ -60,7 +60,7 @@ $relcount = 1;
         $filename = $db_field['filename'];
 
 
-        echo "<tr valign='middle'><td>" . $name . "</td><td><center><a href='http://" . $ip . "' target='_blank'>" . $ip . "</a></td><td><center><img src='" . (strlen($password) > 0 ? 'lock.png' : 'lock-open-variant.png') . "'></td><td><center>" . $version . "</td><td><center>" . $lastbackup . "</td><Td><center><form method='POST' action='index.php'><input type='hidden' value='" . $name . "' name='name'><input type='hidden' value='noofbackups' name='task'><input type='submit' value='" . $numberofbackups . "' class='btn-xs btn-info'></form></td><td><center><form method='POST' action='index.php'><input type='hidden' value='" . $ip . "' name='ip'><input type='hidden' value='singlebackup' name='task'><input type='submit' value='Backup' class='btn-xs btn-success'></form></td><td><center><form method='POST' action='edit.php'><input type='hidden' value='" . $ip . "' name='ip'><input type='hidden' value='" . $name . "' name='name'><input type='hidden' value='edit' name='task'><input type='submit' value='Edit' class='btn-xs btn-warning'></form></td><td><center><form method='POST' action='index.php'><input type='hidden' value='" . $ip . "' name='ip'><input type='hidden' value='" . $name . "' name='name'><input type='hidden' value='delete' name='task'><input type='submit' value='Delete' class='btn-xs btn-danger'></form></td></tr>";
+        echo "<tr valign='middle'><td>" . $version . "</td><td><center>" . $date . "</td><td><a href='" . $filename . "'>DOWNLOAD</a></td></tr>;
         $relcount = $relcount + 1;
     }
 

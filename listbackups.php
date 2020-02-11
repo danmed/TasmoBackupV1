4af8be918a6f8975705b21efb7e7ab5f267b7df0
danmed <danmed@gmail.com>
2020-02-11 20:50:22 +0000

    Update listbackups.php

--- listbackups.php
+++ listbackups.php
@@ -77,32 +77,6 @@ $(document).ready(function() {
     </table>
     </div>
 	  
-	  <!-- Modal -->
-<div id="myModal" class="modal fade" role="dialog">
-  <div class="modal-dialog">
-
-    <!-- Modal content-->
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">TasmoBackup</h4>
-      </div>
-      <div class="modal-body">
-        <p><center>
-          <?php if (isset($output)) {
-    echo $output;
-} ?>
-          <br>
-          <?php if (isset($output2)) {
-    echo $output2;
-} ?>
-        </p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-
   </div>
 </div>
 	  
